5b3e3bef773c04ca659d5c61a616bd49cf962c2c
Mohd Saqiib <edovastudios@gmail.com>
2022-10-18 18:43:11 +0530

    3 Enemy Type Working + Refactoring
    -3 types of enemies can now collide with the player.
    - Refactored the PlayerStateCol using Switch statements and enums.

--- Assets/Scripts/Player/PlayerStateCol.cs
+++ Assets/Scripts/Player/PlayerStateCol.cs
@@ -4,12 +4,20 @@ using UnityEngine.Serialization;
 public class PlayerStateCol : MonoBehaviour
 {
     private readonly string _defaultState = "DefaultState";
-    private readonly string _stateOne = "StateOne";
-    private readonly string _stateSecond = "StateSecond";
+    private readonly string _stateX = "StateOne";
+    private readonly string _stateY = "StateSecond";
 
     [SerializeField] private Shake _shakeScript;
-    
 
+    enum PlayerState
+    {
+        DefaultState,
+        StateX,
+        StateY
+    }
+
+    private PlayerState _playerState;
+    
     [FormerlySerializedAs("_BlueParticleEffect")] [SerializeField] private GameObject _xEnemyParticleEffect;
     [FormerlySerializedAs("_RedParticleEffect")] [SerializeField] private GameObject _yEnemyParticleEffect;
     [SerializeField] private GameObject _zEnemyParticleEffect;
@@ -20,67 +28,59 @@ public class PlayerStateCol : MonoBehaviour
     public GameEvent onGameOver;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (gameObject.name == _defaultState)
-        {
-            if (other.CompareTag("EnemyOne") || other.CompareTag("EnemySecond"))
-            {
-                // Player Die
-                transform.parent.gameObject.SetActive(false);
-
-                onGameOver.Raise();
-                
-                // Screen Shake
-                // Player Explode particle
-            }
-        }
+        if (gameObject.name == _defaultState) _playerState = PlayerState.DefaultState;
+        if (gameObject.name == _stateX) _playerState = PlayerState.StateX;
+        if (gameObject.name == _stateY) _playerState = PlayerState.StateY;
         
-        if (gameObject.name == _stateOne)
+        switch (_playerState)
         {
-            if (other.CompareTag("EnemyOne"))
-            {
-                _animator.SetTrigger("Attack");
-                _shakeScript.CamShake();
-
-                // Play the particles
-                _xEnemyParticleEffect.SetActive(true);
-                _xEnemyParticleEffect.GetComponent<ParticleSystem>().Play();
+            case PlayerState.DefaultState:
+                if (other.CompareTag("EnemyOne") || other.CompareTag("EnemySecond")) GameOver();
+                if (other.CompareTag("EnemyDefault")) EnemyKill(_zEnemyParticleEffect, other);
+                break;
+            
+            case PlayerState.StateX:
+                if (other.CompareTag("EnemyOne")) EnemyKill(_xEnemyParticleEffect, other);
+                if (other.CompareTag("EnemyDefault")) GameOver();
+                break;
+            
+            case PlayerState.StateY:
+                if (other.CompareTag("EnemySecond")) EnemyKill(_yEnemyParticleEffect, other);
+                if (other.CompareTag("EnemyDefault")) GameOver();
+                break;
                 
-                // Enemy Die Sound
-                EnemyDieSound();
-
-                // Event call send
-                onScoreIncreased.Raise();
-
-                var enemyTransform = other.gameObject.GetComponent<Transform>();
-                enemyTransform.position = new Vector3(enemyTransform.position.x - 10, enemyTransform.position.y);
-
-            }
         }
-        
-        if (gameObject.name == _stateSecond)
-        {
-            if (other.CompareTag("EnemySecond"))
-            {
-                _animator.SetTrigger("Attack");
-                _shakeScript.CamShake();
-                
-                // Play the Particle System
-                _yEnemyParticleEffect.SetActive(true);
-                _yEnemyParticleEffect.GetComponent<ParticleSystem>().Play();
-                
-                // Enemy Die Sound
-                EnemyDieSound();
+    }
 
-                // Event call send
-                onScoreIncreased.Raise();
+    private void GameOver()
+    {
+        // Player Die
+        transform.parent.gameObject.SetActive(false);
+
+        // Actual Game Over event send.
+        onGameOver.Raise();
+
+        // Screen Shake
+        // Player Explode particle
+    }
+
+    private void EnemyKill(GameObject enemyDieParticlePrefab, Collider2D other)
+    {
+        _animator.SetTrigger("Attack");
+        _shakeScript.CamShake();
+
+        // Play the particles
+        enemyDieParticlePrefab.SetActive(true);
+        enemyDieParticlePrefab.GetComponent<ParticleSystem>().Play();
                 
-                var enemyTransform = other.gameObject.GetComponent<Transform>();
-                enemyTransform.position = new Vector3(enemyTransform.position.x - 10, enemyTransform.position.y);
-                
-                
-            }
-        }
-        
+        // Enemy Die Sound
+        EnemyDieSound();
+
+        // Event call send
+        onScoreIncreased.Raise();
+
+        var enemyTransform = other.gameObject.GetComponent<Transform>();
+        enemyTransform.position = new Vector3(enemyTransform.position.x - 10, enemyTransform.position.y);
     }
 
     private static void EnemyDieSound()
